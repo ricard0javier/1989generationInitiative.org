bf74af29b225cac8db69cd731dacf00e3a74afef
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2016-11-30 23:47:51 +0000

    update who we are view

--- src/app/view/who_we_are.js
+++ src/app/view/who_we_are.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import SubHeader from './subHeader';
 
-let memberReverse = false;
 const styles = {
   container: {
     padding: '1em 0 1em 0',
@@ -15,12 +14,6 @@ const styles = {
     display: 'flex',
     flexWrap: 'wrap'
   },
-  memberText: {
-    padding: "1em",
-    textAlign: 'left',
-    width: '50%',
-    height: '100%'
-  },
   memberImage: {
     width: '50%',
     height: '100%'
@@ -29,10 +22,15 @@ const styles = {
     color: '#B19D64',
     fontSize: "1em"
   },
-  iconBox(index) {
-    if (index % 2 === 0) {
-      memberReverse = !memberReverse;
-    }
+  memberText(memberReverse) {
+    return {
+      padding: "1em",
+      textAlign: memberReverse ? 'right' : 'left',
+      width: '50%',
+      height: '100%'
+    };
+  },
+  iconBox(memberReverse) {
     return {
       display: 'flex',
       textAlign: 'center',
@@ -44,43 +42,49 @@ const styles = {
 
 const teamContainers = [
   {
-    name: "Team Title",
+    name: "Core Committee",
     members: [
       {
-        name: "Name Here",
-        description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.",
-        linkedin: "#",
-        image: "images/member_default.png"
+        name: "Antonios Fiala",
+        description: "General Secretary",
+        linkedin: "https://uk.linkedin.com/pub/antonios-fiala/85/701/453",
+        image: "images/team_member_antonios_fiala.jpg"
       },
       {
-        name: "Name Here",
-        description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.",
-        linkedin: "#",
-        image: "images/member_default.png"
+        name: "Barbara Aubin",
+        description: "Head of Content",
+        linkedin: "https://www.linkedin.com/in/barbara-aubin-3302a491",
+        image: "images/team_member_barbara_aubin.jpg"
       },
       {
-        name: "Name Here",
-        description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.",
-        linkedin: "#",
-        image: "images/member_default.png"
+        name: "Dominik Kirchdorfer",
+        description: "Head of Expansion",
+        linkedin: "https://www.linkedin.com/in/dfkirchdorfer/en",
+        image: "images/team_member_dominik_kirchdorfer.jpg"
       },
       {
-        name: "Name Here",
-        description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.",
-        linkedin: "#",
-        image: "images/member_default.png"
+        name: "Martin Schmidler",
+        description: "Head of Public Relations",
+        linkedin: "http://www.linkedin.com/in/martin-schmidler",
+        image: "images/team_member_martin_scmidler.jpg"
       },
       {
-        name: "Name Here",
-        description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.",
-        linkedin: "#",
-        image: "images/member_default.png"
+        name: "Michael Cottakis",
+        description: "President",
+        linkedin: "https://uk.linkedin.com/pub/michael-cottakis/b6/985/5b4",
+        image: "images/team_member_michael_cottakis.jpg"
       },
       {
-        name: "Name Here",
-        description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.",
-        linkedin: "#",
-        image: "images/member_default.png"
+        name: "Nicholas Charalambides",
+        description: "VP Treasury",
+        linkedin: "https://uk.linkedin.com/in/nicholascharalambides",
+        image: "images/team_member_nicholas_charalambides.jpg"
+      },
+      {
+        name: "Elena Turci",
+        description: "Head of Events",
+        linkedin: "https://be.linkedin.com/in/eturci/de",
+        image: "images/team_member_elena_turci.jpg"
       }
     ]
   }
@@ -90,25 +94,33 @@ const WhoWeAreComponent = () => (
   <div style={styles.container}>
     <SubHeader pageName="WHO WE ARE"/>
     <div>
-      {teamContainers.map((teamContainer, teamContainerIndex) => (
-        <div key={teamContainerIndex} style={styles.teamContainer}>
-          <h4>{teamContainer.name}</h4>
-          <div style={styles.membersContainer}>
-            {teamContainer.members.map((member, memberIndex) => (
-              <div key={memberIndex} style={styles.iconBox(memberIndex)}>
-                <div style={styles.memberImage}>
-                  <img src={member.image}/>
-                </div>
-                <div style={styles.memberText}>
-                  <h5>{member.name}</h5>
-                  <p>{member.description}</p>
-                  <span><a style={styles.link} href="https://www.linkedin.com/company/9387318"><i className="fa fa-linkedin-square fa-2x"/></a></span>
-                </div>
-              </div>
-            ))}
+      {teamContainers.map((teamContainer, teamContainerIndex) => {
+        let memberReverse = false;
+        return (
+          <div key={teamContainerIndex} style={styles.teamContainer}>
+            <h4>{teamContainer.name}</h4>
+            <div style={styles.membersContainer}>
+              {teamContainer.members.map((member, memberIndex) => {
+                if (memberIndex % 2 === 0) {
+                  memberReverse = !memberReverse;
+                }
+                return (
+                  <div key={memberIndex} style={styles.iconBox(memberReverse)}>
+                    <div style={styles.memberImage}>
+                      <img src={member.image}/>
+                    </div>
+                    <div style={styles.memberText(memberReverse)}>
+                      <h5>{member.name}</h5>
+                      <p>{member.description}</p>
+                      <span><a style={styles.link} target="_blank" href={member.linkedin}><i className="fa fa-linkedin-square fa-2x"/></a></span>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   </div>
 );
