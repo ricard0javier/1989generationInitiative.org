5994bbb7a00313964e8c5693a554a9dc9fae1cd2
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2016-12-21 00:33:08 +0000

    renaming to Collaborators.js

--- src/app/view/Collaborators.js
+++ src/app/view/Collaborators.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {SubHeaderComponent} from './SubHeader';
+import SubHeaderComponent from './SubHeader';
 
 const collaborators = [
   {
--- src/app/view/Footer.js
+++ src/app/view/Footer.js
@@ -28,7 +28,7 @@ const styles = {
 const Footer = () => (
   <div>
     <div style={styles.container}>
-      <HeaderResponsive/>
+      <Header/>
     </div>
     <div className="container-fluid">
       <div className="row" style={styles.containerBottom}>
