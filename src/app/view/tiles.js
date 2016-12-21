72458e183969f9320fb9f0aac33042ce9f5b59e2
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2016-12-21 00:11:52 +0000

    renaming to Footer.js

--- src/app/view/tiles.js
+++ src/app/view/tiles.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from "react";
 import HeaderResponsive from "./headerResponsive";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 const styles = {
   container: {
