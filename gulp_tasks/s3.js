8cac532522b90f71105dcc73f88d6f98ffd2d96e
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2016-12-26 13:08:11 +0200

    building the initial approach for promoting content to live

--- gulp_tasks/s3.js
+++ gulp_tasks/s3.js
@@ -8,15 +8,31 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-gulp.task('cleanS3Bucket', cleanS3Bucket);
+gulp.task('cleanS3Bucket', cleanS3BucketTask);
+gulp.task('cloneS3BucketContent', cloneS3BucketContentTask);
+
+function cleanS3BucketTask(done) {
+  cleanBucket(util.env.bucketName);
+  done();
+}
+
+function cloneS3BucketContentTask(done) {
+  const fromBucketName = util.env.fromBucketName;
+  const toBucketName = util.env.toBucketName;
+
+  cleanBucket(toBucketName);
+  const objects = getObjects(fromBucketName);
+  copyObjects(objects, fromBucketName, toBucketName);
+
+  done();
+}
 
 /**
  * Delete all the S3 Bucket objects contained on the argv[bucketName] bucket
  */
-function cleanS3Bucket(done) {
+function getObjects(bucketName) {
 
-  const objectsToDelete = [];
-  const bucketName = util.env.bucketName;
+  const objects = [];
 
   console.log(`getting list of objects in the bucket '${bucketName}'`);
   // retrieve all objects
@@ -24,32 +40,63 @@ function cleanS3Bucket(done) {
 
   listPromise.then(data => {
     data.Contents.map(object => {
-      objectsToDelete.push({Key: object.Key});
-      console.log(`Object '${object.Key}' marked for deletion`);
+      objects.push(object.Key);
     });
-
-    // delete them
-    deleteS3Objects(bucketName, objectsToDelete);
-    done();
   }).catch((error) => {
     console.log(error);
-    done();
   });
+
+  return objects;
 }
 
 /**
- * Delete and logs the total number of objects deleted
- * @param bucketName
- * @param objectsToDelete
+ * Deletes all the S3 Bucket objects contained on the a given bucket
  */
-const deleteS3Objects = (bucketName, objectsToDelete) => {
-  if (objectsToDelete.length === 0) {
+function cleanBucket(bucketName) {
+
+  const objects = [];
+
+  getObjects(bucketName).map(object => {
+    objects.push({Key: object});
+    console.log(`Object '${object.Key}' marked for deletion`);
+  });
+
+  // delete them
+  deleteObjects(bucketName, objects);
+}
+
+/**
+ * Deletes objects from the bucket
+ * @param bucketName
+ * @param objects
+ */
+const deleteObjects = (bucketName, objects) => {
+  if (objects.length === 0) {
     console.log("There are not objects to delete");
     return;
   }
   console.log(`deleting objects from the bucket '${bucketName}'`);
-  const promise = s3.deleteObjects({Bucket: bucketName, Delete: {Objects: objectsToDelete}}).promise();
+  const promise = s3.deleteObjects({Bucket: bucketName, Delete: {Objects: objects}}).promise();
   promise.then(() => {
-    console.log(`${objectsToDelete.length} elements deleted`)
+    console.log(`${objects.length} elements deleted`)
   });
+};
+
+/**
+ * Copies all the objects from one bucket to other
+ * @param objects
+ * @param fromBucketName
+ * @param toBucketName
+ */
+const copyObjects = (objects, fromBucketName, toBucketName) => {
+  if (objects.length === 0) {
+    console.log("There are not objects to copy");
+    return;
+  }
+  objects.map(key => {
+    const copySource = `${fromBucketName}/${key}`;
+    const copyPromise = s3.copyObject({Bucket: toBucketName, Key: key, CopySource: copySource}).promise();
+    copyPromise.then(() => console.log(`Object '${copySource}' copied to bucket '${toBucketName}'`));
+  });
+  console.log(`${objects.length} elements copied`)
 };
